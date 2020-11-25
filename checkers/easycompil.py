f83c2bb2707db3d8047974cb67a9c586284b99e0
Kai-Builder <gkai@gmail.com>
2020-11-25 11:59:12 -0500

    starting compilers and checkers, added init for fixed import

---
+++ checkers/easycompil.py
@@ -0,0 +1,11 @@
+import time
+class PythonCompiler():
+    class ReadAbleSyntax():
+            def Link(waitfor, chainProsperity):
+                time.sleep(waitfor)
+                class chainProsperityKeeper(chainProsperity):
+                    for int in chainProsperity:
+                        print('+1 Strength')
+
+    class Reacher():
+        
