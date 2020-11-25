d9ac1f7627f2f3a6e1c3d79a59344682dd61164e
Kai-Builder <gkai@gmail.com>
2020-11-25 18:12:48 -0500

    working on shared Libraries

--- KoBashToolkit/binary/library.py
+++ KoBashToolkit/binary/library.py
@@ -1,1 +1,8 @@
 from KoBashToolkit.engine.enginestart import Engine
+
+class Library():
+    class Functions():
+        kk_help = '$help'
+        kk_help2 = '$help 2'
+    class Scripts():
+        localScript = True
